a363e42c80537a45eea52136c3ede332be422ec6
nagazam21 <nagazam21@naver.com>
2024-12-12 17:17:52 +0000

    web config update

--- src/main/java/com/example/WebConfig.java
+++ src/main/java/com/example/WebConfig.java
@@ -9,8 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+       registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("*") // 모든 오리진 허용
+                .allowedMethods("*") // 모든 HTTP 메서드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(false); // 쿠키를 포함한 인증 정보는 허용하지 않음
     }
 }
