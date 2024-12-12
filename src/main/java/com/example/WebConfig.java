cc7664350fe99dfc5cf5f1f5b2b52f794c40318d
SangGuk Lee <nagazam21@naver.com>
2024-12-13 01:17:05 +0900

    Update WebConfig.java

--- src/main/java/com/example/WebConfig.java
+++ src/main/java/com/example/WebConfig.java
@@ -7,12 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
+	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("*") // 모든 오리진 허용
-                .allowedMethods("*") // 모든 HTTP 메서드 허용
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(false); // 쿠키를 포함한 인증 정보는 허용하지 않음
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
